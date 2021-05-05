073e6d7002e1024bf65fd03bbd699f09a0bccc5a
Joe <joncius224@gmail.com>
2021-05-05 18:35:31 +0300

    Lexture #12 (Images handling)

---
+++ image.py
@@ -0,0 +1,25 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+#from kivy.uix.image import Image       #IF YOU USE IMAGES IN PYTHON FILE
+
+
+# DESIGNATE OUR .kv DESIGN FILE
+Builder.load_file('image.kv')          # PIRMASIS BUDAS
+
+
+class MyLayout(Widget):
+    pass
+   
+
+class AwesomeApp(App):
+    def build(self):
+        Window.clearcolor = (1,1,1,1,)
+        return MyLayout()
+
+
+if __name__ == "__main__":
+    AwesomeApp().run()
